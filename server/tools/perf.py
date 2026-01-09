39ad146ca143e5f979c8a21df78324a970f2afc7
leefrank9527 <leefrank9527@gmail.com>
2026-01-10 11:53:00 +1300

    got rid of the secrets

--- server/tools/perf.py
+++ server/tools/perf.py
@@ -3,8 +3,7 @@ import time
 
 if __name__ == "__main__":
     sessions = {}
-    url = "http://localhost:7071/runtime/webhooks/durabletask/instances/6ef83398-7610-4f99-bf11-d44772797db1?code=PFaQsr_l5-fLZ-MV9wpnuZiFhqNE-ClDCLURr9S7ExJ8AzFuIXfgPA=="
-
+    url = "http://example.com/some/very/long/url/that/uses/memory"
     for i in range(100 * 1024 * 1024):
         sessions[i] = (i, dt.now().timestamp(), url)
         if i % (1024 * 1024) == 0:
