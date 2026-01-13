8d8d60cca0a69e2ed7220a00a8cf201bab3daf4e
leefrank9527 <leefrank9527@gmail.com>
2026-01-14 11:48:01 +1300

    ignored the ssl verification and set the log levels to info

--- src/main/java/nz/govt/natlib/dashboard/common/core/RosettaRestApi.java
+++ src/main/java/nz/govt/natlib/dashboard/common/core/RosettaRestApi.java
@@ -6,20 +6,76 @@ import nz.govt.natlib.dashboard.domain.entity.EntityDepositAccountSetting;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509ExtendedTrustManager;
+import java.net.Socket;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
 import java.util.Base64;
 import java.util.List;
 
 public class RosettaRestApi {
     private static final Logger log = LoggerFactory.getLogger(RosettaRestApi.class);
+
+    private final static TrustManager[] trustAllCerts = new TrustManager[]{
+            new X509ExtendedTrustManager() {
+                @Override
+                public void checkClientTrusted(X509Certificate[] chain, String authType, Socket socket) {
+                }
+
+                @Override
+                public void checkServerTrusted(X509Certificate[] chain, String authType, Socket socket) {
+                }
+
+                @Override
+                public void checkClientTrusted(X509Certificate[] chain, String authType, javax.net.ssl.SSLEngine engine) {
+                }
+
+                @Override
+                public void checkServerTrusted(X509Certificate[] chain, String authType, javax.net.ssl.SSLEngine engine) {
+                }
+
+                @Override
+                public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                    return new X509Certificate[0];
+                }
+
+                @Override
+                public void checkClientTrusted(X509Certificate[] chain, String authType) {
+                }
+
+                @Override
+                public void checkServerTrusted(X509Certificate[] chain, String authType) {
+                }
+            }
+    };
+
+
+    private final SSLContext sslContext;
+
     private final String rosettaRestApiUrl;
     private String cookie = "";
 
+    static {
+        System.setProperty("jdk.internal.httpclient.disableHostnameVerification", "true");
+    }
+
+
     public RosettaRestApi(String rosettaRestApiUrl) {
         this.rosettaRestApiUrl = rosettaRestApiUrl;
+        try {
+            this.sslContext = SSLContext.getInstance("TLS");
+            this.sslContext.init(null, trustAllCerts, new SecureRandom());
+        } catch (NoSuchAlgorithmException | KeyManagementException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private String getBasicAuthenticationHeader(EntityDepositAccountSetting depositAccount) {
@@ -36,10 +92,12 @@ public class RosettaRestApi {
             json = ow.writeValueAsString(reqBody);
         }
 
-        HttpClient client = HttpClient.newHttpClient();
+//        HttpClient client = HttpClient.newHttpClient();
+        HttpResponse<String> rsp;
+        HttpClient client = HttpClient.newBuilder().sslContext(this.sslContext).build();
         HttpRequest req = HttpRequest.newBuilder().method(method, HttpRequest.BodyPublishers.ofString(json)).uri(new URI(this.rosettaRestApiUrl + path)).header("Authorization", this.getBasicAuthenticationHeader(depositAccount)).header("Accept", "application/json").header("Content-Type", "application/json").header("Cookie", this.cookie).build();
+        rsp = client.send(req, HttpResponse.BodyHandlers.ofString());
 
-        HttpResponse<String> rsp = client.send(req, HttpResponse.BodyHandlers.ofString());
         if (rsp.statusCode() != 200) {
             String err = String.format("Failed to request: %s, error: %s. Account: %s", path, rsp.body(), depositAccount);
             log.error(err);
