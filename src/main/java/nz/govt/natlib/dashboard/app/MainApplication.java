a27ff7f04199771a67364ed468a3bd96649711c9
leefrank9527 <leefrank9527@gmail.com>
2026-01-14 17:29:04 +1300

    inspected the properties

--- src/main/java/nz/govt/natlib/dashboard/app/MainApplication.java
+++ src/main/java/nz/govt/natlib/dashboard/app/MainApplication.java
@@ -1,6 +1,8 @@
 package nz.govt.natlib.dashboard.app;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,23 +11,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.EnumerablePropertySource;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"nz.govt.natlib.dashboard.app", "nz.govt.natlib.dashboard.common.auth", "nz.govt.natlib.dashboard.domain.daemon", "nz.govt.natlib.dashboard.domain", "nz.govt.natlib.dashboard.ui"})
 public class MainApplication implements CommandLineRunner {
     private static final Logger log = LoggerFactory.getLogger(MainApplication.class);
-
-    public static final String ANSI_RESET = "\u001B[0m";
-    public static final String ANSI_BLUE = "\u001B[34m";
-    //    public static final String ANSI_RED = "\u001B[31m";
-    public static final String ANSI_GREEN = "\u001B[32m";
-    public static final String ANSI_CYAN = "\u001B[36m";
-
     //    @Autowired
     private final ApplicationContext ctx;
+    private final ConfigurableEnvironment env;
 
-    public MainApplication(ApplicationContext ctx) {
+    public MainApplication(ApplicationContext ctx, ConfigurableEnvironment env) {
         this.ctx = ctx;
+        this.env = env;
     }
 
     public static void main(String[] args) {
@@ -39,16 +38,26 @@ public class MainApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         // Note that this is just here for debugging purposes. It can be deleted at any time.
-//        System.out.println(ANSI_GREEN + "Let's inspect the beans provided by Spring Boot:" + ANSI_RESET);
         log.info("Let's inspect the beans provided by Spring Boot:");
         String[] beanNames = ctx.getBeanDefinitionNames();
         Arrays.sort(beanNames);
         for (String beanName : beanNames) {
-//            System.out.println(ANSI_CYAN + beanName + ANSI_RESET);
             log.info("bean: {}", beanName);
         }
 
-//        System.out.println(ANSI_BLUE + "^_^_^_^_^_^_^_^_^_^ Dashboard Initialed ^_^_^_^_^_^_^_^_^_^_^" + ANSI_RESET);
+        List<String> listProps = new ArrayList<>();
+        for (var propertySource : env.getPropertySources()) {
+            if (propertySource instanceof EnumerablePropertySource eps) {
+                for (String name : eps.getPropertyNames()) {
+                    listProps.add(name + "=" + eps.getProperty(name));
+                }
+            }
+        }
+        String[] props = listProps.toArray(new String[0]);
+        Arrays.sort(props);
+        for (String prop : props) {
+            log.info("prop: {}", prop);
+        }
         log.info("^_^_^_^_^_^_^_^_^_^ Dashboard Initialed ^_^_^_^_^_^_^_^_^_^_^");
     }
 }
