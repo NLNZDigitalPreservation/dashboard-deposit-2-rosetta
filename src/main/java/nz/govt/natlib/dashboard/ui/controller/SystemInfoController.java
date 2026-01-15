9181646a60eb7153a587c5b50b97a07e8638535f
leefrank9527 <leefrank9527@gmail.com>
2026-01-16 09:45:58 +1300

    recoverd the gradle

--- src/main/java/nz/govt/natlib/dashboard/ui/controller/SystemInfoController.java
+++ src/main/java/nz/govt/natlib/dashboard/ui/controller/SystemInfoController.java
@@ -3,7 +3,7 @@ package nz.govt.natlib.dashboard.ui.controller;
 import nz.govt.natlib.dashboard.common.DashboardConstants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.info.BuildProperties;
+// import org.springframework.boot.info.BuildProperties;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,18 +14,25 @@ import java.util.Map;
 
 @RestController
 public class SystemInfoController {
-    @Autowired
-    private BuildProperties buildProperties;
+    // @Autowired
+    // private BuildProperties buildProperties;
 
-    @Value("${system.deployment:dev}")
-    private String systemDeployment;
+    @Value("${app.deployment:dev}")
+    private String appDeployment;
+
+    @Value("${app.version:1.0.0}")
+    private String version;
+
 
     @RequestMapping(path = DashboardConstants.SYSTEM_INFO, method = {RequestMethod.GET})
     public ResponseEntity<?> get() {
         try {
             Map<String, String> sysInfo = new HashMap<>();
-            sysInfo.put("version", buildProperties.getVersion());
-            sysInfo.put("systemDeployment", systemDeployment);
+            // String version = getClass().getPackage().getImplementationVersion();
+
+            sysInfo.put("version", version);
+            sysInfo.put("systemDeployment", appDeployment);
+
             return ResponseEntity.ok().body(sysInfo);
         } catch (Exception e) {
             return ResponseEntity.badRequest().body(e.getMessage());
