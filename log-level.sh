3d7874f62edf1f90b17ec0f35668c3b2a94faff1
leefrank9527 <leefrank9527@gmail.com>
2026-07-27 23:17:32 +1200

    updated the context path to depdash

--- log-level.sh
+++ log-level.sh
@@ -2,16 +2,16 @@
 
 #Checking the current log level:
 echo "Before, the log level is:"
-curl -X "GET" "http://localhost:1901/deposit-dashboard/actuator/loggers/nz.govt.natlib.dashboard"
+curl -X "GET" "http://localhost:1901/depdash/actuator/loggers/nz.govt.natlib.dashboard"
 echo ""
 
 #Configure the log level
 echo "Going to reset log level"
-curl -X "POST" "http://localhost:1901/deposit-dashboard/actuator/loggers/nz.govt.natlib.dashboard" \
+curl -X "POST" "http://localhost:1901/depdash/actuator/loggers/nz.govt.natlib.dashboard" \
      -H "Content-Type: application/json; charset=utf-8" \
      -d $'{  "configuredLevel": "DEBUG"}'
 echo ""
 
 #Checking the current log level:
 echo "Now, the log level is:"
-curl -X "GET" "http://localhost:1901/deposit-dashboard/actuator/loggers/nz.govt.natlib.dashboard"
+curl -X "GET" "http://localhost:1901/depdash/actuator/loggers/nz.govt.natlib.dashboard"
